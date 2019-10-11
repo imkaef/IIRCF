987a657f852b6633df8bf7592a1908b84d2a9b60
Sergey <Sergey.popov.fly@gmail.com>
2019-10-11 13:45:01 +0300

    Create application

---
+++ app/src/main/java/com/psb/IIRCF/TopLevelActivity.kt
@@ -0,0 +1,12 @@
+package com.psb.IIRCF
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TopLevelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_level)
+    }
+}
