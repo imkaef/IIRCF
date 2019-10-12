488e889ee21681ba6ce56b2d6a087b9b52f5da31
Sergey <Sergey.popov.fly@gmail.com>
2019-10-12 09:38:22 +0300

    Add coffee pictures and create class DRINKS

--- app/src/main/java/com/psb/IIRCF/Drink.kt
+++ app/src/main/java/com/psb/IIRCF/Drink.kt
@@ -1,2 +1,27 @@
 package com.psb.IIRCF
 
+class Drink private constructor(
+    val name: String,
+    val description: String,
+    val imageResourceId: Int
+) {
+    override fun toString(): String {
+        return this.name
+    }
+
+    companion object {
+        //drinks is an array of Drinks
+        val drinks = arrayOf(
+            Drink(
+                "Latte", "A couple of espresso shots with steamed milk",
+                R.drawable.latte
+            ), Drink(
+                "Cappuccino", "Espresso, hot milk, and a steamed milk foam",
+                R.drawable.cappuccino
+            ), Drink(
+                "Filter", "Highest quality beans roasted and brewed fresh",
+                R.drawable.filter
+            )
+        )    //Each Drink has a name, description, and an image resource
+    }
+}
--- app/src/main/java/com/psb/IIRCF/TopLevelActivity.kt
+++ app/src/main/java/com/psb/IIRCF/TopLevelActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class TopLevelActivity : AppCompatActivity() {
-
+//Code | Convert Java File to Kotlin File Ctrl+Shift+Alt+K.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_level)
