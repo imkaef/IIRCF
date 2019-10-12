596653f21986a24cce41002adcdcd514b85dcab4
Sergey <Sergey.popov.fly@gmail.com>
2019-10-12 14:31:32 +0300

    Create 2 Activity for choise coffee

---
+++ app/src/main/java/com/psb/IIRCF/DrinkCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.psb.IIRCF
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrinkCategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drink_category)
+    }
+}
--- app/src/main/java/com/psb/IIRCF/TopLevelActivity.kt
+++ app/src/main/java/com/psb/IIRCF/TopLevelActivity.kt
@@ -1,12 +1,30 @@
 package com.psb.IIRCF
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_top_level.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class TopLevelActivity : AppCompatActivity() {
 //Code | Convert Java File to Kotlin File Ctrl+Shift+Alt+K.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_level)
+    list_options.setOnItemClickListener(){
+            parent, view, position, id ->
+            if(position == 0){
+                //val intent = Intent(this, DrinkCategoryActivity::class.java)
+                //startActivity(intent)
+            }
+
+        }
     }
 }
